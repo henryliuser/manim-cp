02589b4df0a1ceed5d141377b6ded027d8d100b3
Samuel Brashears <samuelbrashears@Samuels-MacBook-Pro.local>
2022-08-21 17:10:45 -0500

    not much

--- projects/2-cows/p1.py
+++ projects/2-cows/p1.py
@@ -4,5 +4,5 @@ from common import *
 
 class p1(Scene):
     def construct(self):
-
+        a = [(1, 25), (10, 6), (5, 23), (18, 26), (7, 27), (23, 16), (22, 19), (12, 20), (21, 11), (0, 14)]
         self.wait(1)
